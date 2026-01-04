56277220fee81afabdac75340ba31c0c136ed3b9
Suresh <mskmouni@gmail.com>
2026-01-04 21:11:47 +0530

    Arjun psshing code

--- src/test/java/TestCases/Tests/LoginTest.java
+++ src/test/java/TestCases/Tests/LoginTest.java
@@ -88,6 +88,27 @@ public class LoginTest {
 		
 	}
 	
+	@Test(groups= {"regression"})
+
+	public void ArjunTestCase() {
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get(TestData.url);
+		driver.findElement(By.id("Courses")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.xpath("//button[@id='select button']")).isDisplayed();
+		driver.close();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 	
 	
 	
