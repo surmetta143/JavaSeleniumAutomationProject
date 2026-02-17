b67c40f84fec129122ff61d859adfebdddedd7a1
surmetta143 <90646511+surmetta143@users.noreply.github.com>
2026-02-17 21:36:57 +0530

    Create AbliashTestCases.java

---
+++ src/test/java/TestCases/Tests/AbliashTestCases.java
@@ -0,0 +1,45 @@
+ @Test(groups= {"ganesh"})
+
+	    void VerifyStopButton()  {
+	    	
+	    	WebDriver driver = new ChromeDriver();
+	    	
+	    	driver.get("https://testautomationpractice.blogspot.com/");
+	    	
+	    	
+	    	
+	    	 WebElement x= driver.findElement(By.xpath("//button[@name='start']"));
+	         
+	    	 x.click();
+	    	 
+	    	
+	            
+	    	driver.findElement(By.xpath("//button[@name='stop']")).isDisplayed();
+	    	
+	    	driver.close();
+	    	
+	    	
+	    	
+	    	
+	    	
+	        
+	    }
+	    
+	    @Test(groups = {"ganesh"})
+	    void VerifySelectButton()  {
+	   
+	    	
+	    	WebDriver driver = new ChromeDriver();
+	    	
+	    	// Step 1: Open application URL
+	    	driver.get("https://jovial-buttercream-da99f3.netlify.app/");
+	    	
+	    	// Step 2: Click on Courses option
+	    	driver.findElement(By.id("Courses")).click();
+	    	
+	    	
+	    	// Step 3: Verify Select button is displayed
+	    	driver.findElement(By.xpath("//button[text()='Select']")).isDisplayed();
+	    	
+	    	driver.close();
+	    }
